a209e95f036d8cee28a933bc1ce21b6cab7ae495
agsrc <akshay0712@gwu.edu>
2019-07-30 17:01:44 -0400

    adding input validation using Joi package

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi'); //is a class -- using for input validaion
 const express =require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses =[
     {id: 1, name: 'course1'},
@@ -21,10 +24,25 @@ if(!course) res.status(404).send('The course with the given id was not found');
 res.send(course);
 });
 
+
+//to handle post 
 app.post('/api/courses',(req,res)=>{
-    const course ={
-        id: courses.length +1
+    const schema = {
+        name: Joi.string().min(3).required()
     };
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course ={
+        id: courses.length +1, 
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+    
 });
 const port = process.env.PORT ||3000;
 app.listen(port, ()=> console.log(`listening at port ${port}...`));
