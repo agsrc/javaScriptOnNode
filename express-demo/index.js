6af14d61508c9968a24904b5d4d86bd29b5ae823
agsrc <akshay0712@gwu.edu>
2019-07-30 22:22:59 -0400

    cleaning code

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,24 @@
 const Joi = require('joi'); //is a class -- using for input validaion
+const config = require('config');
+const morgan = require('morgan')
+const helmet = require('helmet')
+//const logger = require('./logger')
 const express =require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // req.body(midleware)
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+app.use(helmet());
+
+// Configuration
+console.log('Application Name: '+ config.get('name'));
+console.log('Mail Server: '+ config.get('mail.host'));
+if(app.get('env')==='development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
+//app.use(logger);
 
 const courses =[
     {id: 1, name: 'course1'},
@@ -16,7 +32,7 @@ app.get('/',(req,res)=>{ // root of the website and callback function param -- t
     app.get('/api/courses',(req,res)=>{
         res.send([1,2,3]); 
     });
-    
+
     // api/courses/1
 app.get('/api/courses/:id',(req,res)=>{
 const course = courses.find(c=>c.id ===parseInt(req.params.id));
@@ -28,26 +44,23 @@ res.send(course);
 
 //to handle post 
 app.post('/api/courses',(req,res)=>{
-    const {error} =validateCourse(req.body); // result.error -- object destructure
+    const {error} = validateCourse(req.body); // result.error -- object destructure
     if(error)
-        return res.status(400).send(result.error.details[0].message);
-    const course ={
+    return res.status(400).send(error.details[0].message);
+    
+        const course ={
         id: courses.length +1, 
         name: req.body.name
     };
     courses.push(course);
-    res.send(course);
-    
-});
+    res.send(course);});
 
 app.put('/api/courses/:id',(req,res)=>{
     // look up the course
     // If not existing, return 404
     const course = courses.find(c=>c.id ===parseInt(req.params.id));
     if(!course)
-    return res.status(404).send('The course with the given id was not found');
-    
-
+    return res.status(404).send('The course with given id was not found');
     // Validate
     //If invalid, retur 400 - bad request    
     const {error} =validateCourse(req.body); // result.error -- object destructure
@@ -75,10 +88,11 @@ app.delete('/api/courses/:id', (req,res)=>{
 
 
 function validateCourse(course){
-    const schema = {
+    const schema ={
         name: Joi.string().min(3).required()
     };
-    return Joi.validate(course, schema);
+    const result = Joi.validate(course,schema);
+    console.log(course);
 }
 const port = process.env.PORT ||3000;
 app.listen(port, ()=> console.log(`listening at port ${port}...`));
